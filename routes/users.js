8e5507dca16f903b0a8e6980503559d4ca151392
Frosty21 <nathan.froese21@gmail.com>
2017-04-20 14:28:45 -0700

    routes users schema

--- routes/users.js
+++ routes/users.js
@@ -8,3 +8,23 @@ var utils = require('../utils/index');
 var email = require('../utils/email');
 var expressJwt = require('express-jwt');
 
+if (!process.env.JWT_SECRET) {
+  console.error('ERROR!: Please set JWT_SECRET before running the app. \n run: export JWT_SECRET=<some secret string> to set JWTSecret. ')
+  process.exit();
+}
+
+var userSchema = mongoose.Schema({
+  name: String,
+  username: String,
+  email: String,
+  password: String,
+  image: String,
+  admin: Boolean,
+  isEmailVerified: Boolean,
+  verifyEmailToken: String,
+  verifyEmailTokenExpires: Date
+});
+
+userSchema.plugin(timestamps);
+
+var User = mongoose.model('User', userSchema);
